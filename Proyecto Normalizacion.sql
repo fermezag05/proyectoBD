06b145423aa46cf4172a6ebf2da6ff8c42231f23
fermezag05 <60576895+fermezag05@users.noreply.github.com>
2025-05-10 13:13:35 -0600

    Add files via upload
    Normalizacion #2

--- Proyecto Normalizacion.sql
+++ Proyecto Normalizacion.sql
@@ -2,41 +2,37 @@ BEGIN;
 
 CREATE TABLE crime_types (
 	id BIGSERIAL PRIMARY KEY,
-    iucr SMALLINT,
-    primary_type TEXT,
-    description TEXT
+    iucr VARCHAR(200) NOT NULL,
+    primary_type VARCHAR(200) NOT NULL,
+    description TEXT NULL
 );
 
 CREATE TABLE locations (
 	id BIGSERIAL PRIMARY KEY
-    block TEXT,
-    location_description TEXT,
-    district INTEGER,
-    ward INTEGER,
-    community_area INTEGER
+    block VARCHAR(200) NOT NULL,
+    location_description TEXT NOT NULL,
+    district BIGINT NOT NULL,
+    ward BIGINT NOT NULL,
+    community_area BIGINT NOT NULL
 );
 
 CREATE TABLE crimes(
     id BIGSERIAL PRIMARY KEY,
-    case_number VARCHAR(50),
-    "date" TIMESTAMP,
+    case_number VARCHAR(200) NOT NULL,
+    crime_date TIMESTAMP NOT NULL,
     locations_id BIGINT NOT NULL REFERENCES locations(id) ON DELETE CASCADE,
     crimes_types_id BIGINT NOT NULL REFERENCES crime_types(id) ON DELETE CASCADE,
-    arrest BOOLEAN,
-    domestic BOOLEAN,
-    beat INTEGER,
-    fbi_code VARCHAR(50),
-    x_coordinate BIGINT,
-    y_coordinate BIGINT,
-    "year" BIGINT,
-    updated_on TIMESTAMP,
+    arrest BOOLEAN NOT NULL,
+    domestic BOOLEAN NOT NULL,
+    beat BIGINT NOT NULL,
+    fbi_code VARCHAR(200) NOT NULL,
+    x_coordinate BIGINT NOT NULL,
+    y_coordinate BIGINT NOT NULL,
+    "year" BIGINT NOT NULL,
+    updated_on VARCHAR(200),
     latitude DOUBLE PRECISION,
     longitude DOUBLE PRECISION,
-    location TEXT
+    location TEXT NULL
 );
-
-SELECT "Block", "District", "Ward", "Community Area" FROM crimes_2021 ORDER BY "Block" LIMIT 300;
-
-SELECT * FROM crimes_2021 LIMIT 100;
 
 ROLLBACK;
