dd1877d2831d4a6cfafdc111b81c211ecbc8dc29
Jorge Puszkar <jpuszkar@itam.mx>
2025-05-11 13:18:31 -0600

    Update EntregaB.sql

--- EntregaB.sql
+++ EntregaB.sql
@@ -1,3 +1,4 @@
+--En este codigo se vera como se hace la carga inicial de los datos desde un .csv que esta disponible para descargar desde https://data.cityofchicago.org/Public-Safety/Crimes-2021/dwme-t96c/about_data 
 CREATE DATABASE nombre_de_la_base; 
 --Reemplaza nombre_de_la_base con el nombre que desees darle. Después, conéctate a la base de datos usando:
 \c nombre_de_la_base 
@@ -26,7 +27,7 @@ CREATE TABLE staging(id BIGSERIAL PRIMARY KEY,
  longitude DOUBLE PRECISION,
  location VARCHAR(200));
 
-
+--Por último, carga la información utilizando el siguiente comando en SQL Shell:
 /*
 \copy staging(id, case_number, crime_date, block, iucr, primary_type, description, location_description, arrest, domestic, beat, district, ward, community_area, fbi_code, x_coordinate, y_coordinate, year, updated_on, latitude, longitude, location) FROM '/.../Crimes_-_2021_20250416.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
 */
