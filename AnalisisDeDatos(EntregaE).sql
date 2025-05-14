6c17c7f851826bbe4f3d304d14103306be5d1395
Jorge Puszkar <jpuszkar@itam.mx>
2025-05-14 12:18:56 -0600

    Update AnalisisDeDatos(EntregaE).sql

--- AnalisisDeDatos(EntregaE).sql
+++ AnalisisDeDatos(EntregaE).sql
@@ -32,16 +32,28 @@ JOIN crime_codes cc ON c.iucr = cc.iucr
 GROUP BY cc.primary_type
 ORDER BY tasa_arresto_pct DESC;
 ---4
+WITH crimenes_contados AS (
+    SELECT
+        c.district,
+        cc.primary_type,
+        COUNT(*) AS total_crimenes,
+        ROW_NUMBER() OVER (
+            PARTITION BY c.district
+            ORDER BY COUNT(*) DESC
+        ) AS ranking
+    FROM crimes c
+    JOIN crime_codes cc ON c.iucr = cc.iucr
+    WHERE c.district IS NOT NULL
+    GROUP BY c.district, cc.primary_type
+)
+
 SELECT
-  c."year",
-  COUNT(*) AS total_crimes,
-  ROUND(
-    100.0 * COUNT(*) / SUM(COUNT(*)) OVER (),
-    2
-  ) AS pct_sobre_total
-FROM crimes c
-GROUP BY c."year"
-ORDER BY c."year";
+    district,
+    primary_type,
+    total_crimenes
+FROM crimenes_contados
+WHERE ranking <= 3
+ORDER BY district, total_crimenes DESC;
 ---5
 SELECT
   c.community_area,
@@ -57,12 +69,12 @@ ORDER BY pct_domesticos DESC
 LIMIT 10;
 ---6
 SELECT
-  DATE_TRUNC('month', crime_date) AS mes,
+  TO_CHAR(DATE_TRUNC('month', crime_date), 'TMMonth') AS mes,
   COUNT(*) AS mensual,
   SUM(COUNT(*)) OVER (ORDER BY DATE_TRUNC('month', crime_date)) AS acumulado
 FROM crimes
-GROUP BY mes
-ORDER BY mes;
+GROUP BY DATE_TRUNC('month', crime_date)
+ORDER BY DATE_TRUNC('month', crime_date);
 --7 
 
 SELECT crimes.id, coordinates.latitude, coordinates.longitude
